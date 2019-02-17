5e9169c722310becb1977eb9ef8a22be326d1b92
Hunter Casillas <huntercasillas@me.com>
2019-02-16 23:50:17 -0700

    Finished Lab 5

--- Lab 5 - Plinko with Functions/Main.cpp
+++ Lab 5 - Plinko with Functions/Main.cpp
@@ -51,9 +51,8 @@ Program Output: Same
 
 using namespace std;
 
-//--------------------------------Prize Money Function------------------------------------
-double get_prize_money(double location)
-{
+// Prize Money Function
+double get_prize_money(double location) {
 	double prize;
 	const double prize0 = 100;
 	const double prize1 = 500;
@@ -65,8 +64,11 @@ double get_prize_money(double location)
 	const double prize7 = 500;
 	const double prize8 = 100;
 
-	if (location == 0) { prize = prize0; }
-	else if (location == 1) { prize = prize1; }
+	if (location == 0) {
+        prize = prize0;
+        
+    }
+    else if (location == 1) { prize = prize1; }
 	else if (location == 2) { prize = prize2; }
 	else if (location == 3) { prize = prize3; }
 	else if (location == 4) { prize = prize4; }
@@ -76,76 +78,58 @@ double get_prize_money(double location)
 	else if (location == 8) { prize = prize8; }
 
 	return prize;
-
 }
 
-
-//---------------------------Single Chip Function-------------------------------------
-double single_chip(int slot_number, int chip_number)
-{
+// Single Chip Function
+double single_chip(int slot_number, int chip_number) {
 	double location = slot_number;
 	
-	if (slot_number >= 0 && slot_number <= 8) //for loop to simulate random falling of chip
-	{
-		if (chip_number == 1)
-		{
+    // For loop to simulate random falling of chip
+    if (slot_number >= 0 && slot_number <= 8) {
+        if (chip_number == 1) {
 			cout << location << endl;
 		}
-		for (int i = 0; i < 12; i++) // simulate chip drop
-		{
+        // Simulate chip drop
+        for (int i = 0; i < 12; i++) {
 
 			double random = rand() % 2;
-			if (random < 1)
-			{
+            
+            if (random < 1) {
 				location = location + 0.5;
-			}
-			else
-			{
+            } else {
 				location = location - 0.5;
 			}
-			if (location < 0)
-			{
+            
+            if (location < 0) {
 				location = 0.5;
-			}
-			else if (location > 8)
-			{
+            } else if (location > 8) {
 				location = 7.5;
 			}
-			if (chip_number == 1)
-			{
+            
+            if (chip_number == 1) {
 				cout << fixed << setprecision(1) << location << " ";
 			}
 		}
-
-
 	}
-
-
-	return location;//return final position
+	return location;
 }
 
 
-//-----------------------------------------Multiple Chip Function-----------------------------------------
-double multiple_chips(int slot_number, int chip_number)
-{
+// Multiple Chip Function
+double multiple_chips(int slot_number, int chip_number) {
 	double location = slot_number;
 	double totalwinnings = 0;
 
-	if (chip_number > 0)
-	{
-		if (slot_number >= 0 && slot_number <= 8)
-		{
-
-			for (int i = 0; i < chip_number; i++)
-			{
+    if (chip_number > 0) {
+        if (slot_number >= 0 && slot_number <= 8) {
+            for (int i = 0; i < chip_number; i++) {
 				location = single_chip(slot_number, chip_number);
 				totalwinnings = totalwinnings + get_prize_money(location);
 			}
-
 		}
 	}
-
-	double average = totalwinnings / chip_number;// calculate total winnings for multiple chips
+    // Calculate total winnings for multiple chips
+	double average = totalwinnings / chip_number;
 	cout << "\nSlot " << slot_number << endl;
 	cout << "Average winnings per chip: $" << average << endl;
 	cout << "Total winnings: $" << totalwinnings << endl;
@@ -154,122 +138,97 @@ double multiple_chips(int slot_number, int chip_number)
 }
 
 
-//--------------------------------------------Multiple Chips in All Slots Function -----------------------------------
-double all_slots(int slot_number, int chip_number)
-{
+// Multiple Chips in All Slots Function
+double all_slots(int slot_number, int chip_number) {
 	double location = slot_number;
 	double totalwinnings = 0;
 
-	for (int i = 0; i <= 8; i++)
-	{
+    for (int i = 0; i <= 8; i++) {
 		slot_number = i;
 		location = multiple_chips(slot_number, chip_number);
 		totalwinnings = totalwinnings + get_prize_money(location);
-		//cout << "Slot " << slot_number << endl;
-
 	}
+    
 	double average = totalwinnings / chip_number;
 
 	return location;
 }
 
-//--------------------------------------------Main Function------------------------------------------------
-int main()
-{
+// Main Function
+int main() {
 	srand(time(0));
 	int menu = 1;
-	while (menu == 1)
-	{
+    while (menu == 1) {
 		cout << "\nMENU:" << endl;
 		cout << "Please enter one of the following options to continue: \n\tDrop One (1)\n\tDrop Multiple (2)\n\tDrop Multiple into all the slots (3)\n\tQuit (4): ";
 		string menuoption = "";
 		cin >> menuoption;
-		if (cin.fail())
-		{
+        
+        if (cin.fail()) {
 			cin.clear();
 			cin.ignore(1000, '\n');
 			cout << "Invalid Selection. Please enter a number." << endl;
 		}
+        
 		double location = 0;
-		int slot_number = 0;// declare some variables
+		int slot_number = 0;
 		int chip_number = 0;
 
-		if (menuoption == "1")// single chip
-		{
+        if (menuoption == "1") {
 			chip_number = 1;
 			cout << "Select a slot (0-8): ";
 			cin >> slot_number;
 			
-			if (cin.fail())
-			{
+            if (cin.fail()) {
 				cin.clear();
 				cin.ignore(1000, '\n');
 				cout << "Invalid Selection. Please enter a number." << endl;
-			}
-			else if (slot_number >= 0 && slot_number <= 8)
-			{
+                
+            } else if (slot_number >= 0 && slot_number <= 8) {
 				location = single_chip(slot_number, chip_number);
 				cout << endl << "Winnings: $" << get_prize_money(location) << endl;
 			}
-		
-		}
-
-		else if (menuoption == "2")// multiple chips options
-		{
-			cout << "Please enter the number of chips to drop: ";//multiple chips input
+            
+        } else if (menuoption == "2") {
+			cout << "Please enter the number of chips to drop: ";
 			cin >> chip_number;
-			if (cin.fail())
-			{
+            
+            if (cin.fail()) {
 				cin.clear();
 				cin.ignore(1000, '\n');
 				cout << "Invalid Selection. Please enter a number." << endl;
-			}
-			else if (chip_number > 0)
-			{
+                
+            } else if (chip_number > 0) {
 				cout << "Select a slot (0-8): ";
 				cin >> slot_number;
-				if (cin.fail())
-				{
+                
+                if (cin.fail()) {
 					cin.clear();
 					cin.ignore(1000, '\n');
 					cout << "Invalid Selection. Please enter a number." << endl;
-				}
-				else if (slot_number >= 0 && slot_number <= 8)
-				{
+				} else if (slot_number >= 0 && slot_number <= 8) {
 					location = multiple_chips(slot_number, chip_number);
-					/*double average = totalwinnings / chip_number;// calculate total winnings for multiple chips
-					cout << "Average winnings per chip: $" << average << endl;
-					cout << "Total winnings: $" << totalwinnings << endl;*/
 				}
 			}
-		}
-
-		else if (menuoption == "3")//multiple chips into all the slots
-		{
+            
+		} else if (menuoption == "3") {
 			cout << "Please enter the number of chips to drop: ";
 			cin >> chip_number;
-			if (cin.fail())
-			{
+            
+            if (cin.fail()) {
 				cin.clear();
 				cin.ignore(1000, '\n');
 				cout << "Invalid Selection. Please enter a number." << endl;
-			}
-			else if (chip_number > 0)
-			{
+            } else if (chip_number > 0) {
 				location = all_slots(slot_number, chip_number);
 			}
-		}
-
-		else if (menuoption == "4")// quit option
-		{
+            
+		} else if (menuoption == "4") {
 			return 0;
-		}
-
-		else if (menuoption != "1" || menuoption != "2" || menuoption != "3" || menuoption != "4")
-		{
+            
+        } else if (menuoption != "1" || menuoption != "2" || menuoption != "3" || menuoption != "4") {
 			cout << "Invalid Selection. Please enter 1, 2, 3, or 4" << endl << endl;
 		}
-
 	}
 
 	system("pause");
